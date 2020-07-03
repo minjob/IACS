c62c28d53e183bed61e8d21a9a4087a57b9ad9fc
lenux-PC\lenux <minwork0926@163.com>
2020-07-03 17:17:40 +0800

    no message

--- shen-web/src/views/system/Calendar.vue
+++ shen-web/src/views/system/Calendar.vue
@@ -10,11 +10,7 @@
         <div class="scheduleContainer" style="height: 730px;">
           <div id='external-events' class="itemMarginBottom">
             <h4>可拖放的日程</h4>
-            <div id="fcEvent">
-              <draggable element="ul" v-model="scheduleTableData.data">
-              <a class='fc-event' v-for="item in scheduleTableData.data" style="padding: 5px;margin: 10px 0;cursor: pointer;" :style="{ background:item.color }">{{item.DateTypeName}}</a>
-              </draggable>
-            </div>
+            <a class='fc-event' v-for="item in scheduleTableData.data" style="padding: 5px;margin: 10px 0;cursor: pointer;" :color="item.color" :style="{ background:item.color,border:'1px solid ' +item.color }">{{item.DateTypeName}}</a>
           </div>
           <el-popover
             placement="right"
@@ -38,7 +34,6 @@
                         @dateClick="handleDateClick"
                         @eventClick="handleEventClick"
                         @eventDrop="handleEventDrop"
-                        @eventResize="handleEventResize"
                         @drop="drop"
           />
         </div>
@@ -52,15 +47,14 @@
   import FullCalendar from '@fullcalendar/vue'
   import dayGridPlugin from "@fullcalendar/daygrid";
   import timeGridPlugin from "@fullcalendar/timegrid";
-  import interactionPlugin from "@fullcalendar/interaction";
+  import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
   import '@fullcalendar/core/main.css';
   import '@fullcalendar/daygrid/main.css';
   import '@fullcalendar/timegrid/main.css';
-  import draggable from 'vuedraggable'
   var moment = require('moment');
   export default {
     name: "Calendar",
-    components:{tableView,FullCalendar,draggable},
+    components:{tableView,FullCalendar},
     data(){
       return {
         scheduleTableData:{
@@ -110,9 +104,18 @@
     mounted() {
       this.getScheduleTableData()
       this.getScheduling()
+      new Draggable(document.getElementById("external-events"), {
+        itemSelector: '.fc-event',
+        eventData: function(eventEl) {
+          return {
+            title: eventEl.innerText,
+            duration: '02:00'
+          };
+        }
+      });
     },
     methods:{
-      getScheduleTableData(){ //渲染
+      getScheduleTableData(){ //获取日程并实例化可拖动
         var that = this
         var params = {
           tableName: this.scheduleTableData.tableName,
@@ -127,8 +130,9 @@
           that.scheduleTableData.total = data.total
         })
       },
-      getScheduling(){
-        this.axios.get("/api/CUID",{
+      getScheduling(){  //获取日历所有日程
+        let that = this
+        that.axios.get("/api/CUID",{
           params: {
             tableName: "scheduledate",
             limit:100000000,
@@ -136,169 +140,103 @@
           }
         }).then(res =>{
           var data = JSON.parse(res.data)
-          this.events = data.rows
+          that.events = []
+          data.rows.forEach(item =>{
+            that.events.push({
+              ID:item.ID,
+              title:item.DateType,
+              color:item.color,
+              start:item.WorkDate
+            })
+          })
         })
-          // $("#fcEvent").html("")
-          // that.scheduleTableData.data.forEach(item =>{
-          //   $("#fcEvent").append(`<a class='fc-event' style="padding: 5px;border:1px solid ${item.color};background:${item.color};margin: 10px 0;cursor: pointer;">${item.DateTypeName}</a>`)
-          // })
-          // $('#fcEvent .fc-event').each(function () {
-          //   $(this).data('event', {
-          //     title: $.trim($(this).text()), // use the element's text as the event title
-          //     stick: false // 拖动后不固定显示在日历上
-          //   });
-          //   // 使事件可拖放
-          //   $(this).draggable({
-          //     zIndex: 999,
-          //     revert: true,
-          //     revertDuration: 0  //拖动后的原始位置
-          //   });
-          // });
-        //   $('#fullcalendar').fullCalendar({
-        //     header: {
-        //       right: 'prev,next today',
-        //       center: 'title',
-        //       left: 'month,agendaWeek,agendaDay,listMonth'
-        //     },
-        //     buttonText: {
-        //       today: '今天',
-        //       month: '月',
-        //       week: '周',
-        //       day: '日',
-        //       list:'日程'
-        //     },
-        //     monthNames: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-        //     monthNamesShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-        //     dayNames: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
-        //     dayNamesShort: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
-        //     editable: true, //支持拖动换位
-        //     navLinks: true, //是否可以单击日/周名称来导航视图
-        //     weekNumbers: true, //是否在日历中显示第几周
-        //     droppable: true, // 允许将事件放在日历上
-        //     height: 700,
-        //     //初始化数据
-        //     events:function(start,end,timezone,callback){
-        //       var dataList= []
-        //       var params = {
-        //         tableName: "scheduledate",
-        //         limit:100000000,
-        //         offset:0
-        //       }
-        //       that.axios.get("/api/CUID",{params: params}).then(res =>{
-        //         var resData = JSON.parse(res.data)
-        //         console.log(resData)
-        //         resData.rows.forEach(item =>{
-        //           dataList.push({
-        //             ID:item.ID,
-        //             title:item.DateType,
-        //             start:item.WorkDate,
-        //             color:item.color
-        //           })
-        //         })
-        //         callback(dataList)
-        //       })
-        //     },
-        //     drop: function(date, allDay) {
-        //       console.log(date)
-        //       var color = ""
-        //       that.scheduleTableData.data.forEach(item =>{
-        //         if(allDay.target.innerHTML === item.DateTypeName){
-        //           color = item.color
-        //         }
-        //       })
-        //       var params = {
-        //         tableName: "scheduledate",
-        //         DateType:allDay.target.innerHTML,
-        //         WorkDate:moment(date._d).format("YYYY-MM-DD"),
-        //         color:color
-        //       }
-        //       that.axios.post("/api/CUID",that.qs.stringify(params)).then(res =>{
-        //         if(res.data == "OK"){
-        //           that.$message({
-        //             showClose: true,
-        //             type: 'success',
-        //             message: "添加成功"
-        //           });
-        //           $('#fullcalendar').fullCalendar('refetchEvents');
-        //         }else{
-        //           that.$message({
-        //             type: 'info',
-        //             message: res.data
-        //           });
-        //         }
-        //       })
-        //     },
-        //     //拖拽换位事件
-        //     eventDrop:function(event,dayDelta,minuteDelta,allDay,revertFund){
-        //       var params = {
-        //         tableName: "scheduledate",
-        //         ID:event.ID,
-        //         DateType:event.title,
-        //         WorkDate:moment(event.start._d).format("YYYY-MM-DD"),
-        //         color:event.color
-        //       }
-        //       that.axios.put("/api/CUID",that.qs.stringify(params)).then(res =>{
-        //         if(res.data == "OK"){
-        //           that.$message({
-        //             showClose: true,
-        //             type: 'success',
-        //             message: '修改成功'
-        //           });
-        //           $('#fullcalendar').fullCalendar('refetchEvents');
-        //         }else{
-        //           that.$message({
-        //             type: 'info',
-        //             message: res.data
-        //           });
-        //         }
-        //       })
-        //     },
-        //     //点击日程事件进行删除
-        //     eventClick:function(event){
-        //       var mulId = [{
-        //         ID:event.ID
-        //       }]
-        //       that.axios.delete("/api/CUID",{
-        //         params: {
-        //           tableName:"scheduledate",
-        //           delete_data:JSON.stringify(mulId)
-        //         }
-        //       }).then(res =>{
-        //         if(res.data == "OK"){
-        //           that.$message({
-        //             showClose: true,
-        //             type: 'success',
-        //             message: '删除成功'
-        //           });
-        //           $('#fullcalendar').fullCalendar('refetchEvents');
-        //         }
-        //       },res =>{
-        //         console.log("请求错误")
-        //       })
-        //     },
-        //     dayClick:function(date, allDay, jsEvent, view){  //点击日期
-        //
-        //     }
-        //   })
-        // },res =>{
-        //   console.log("请求错误")
-        // })
-      },
-      handleDateClick(arg) {  //点击日期 添加休息日
-
-      },
-      handleEventClick(e){
-
-      },
-      handleEventDrop(e){
-
-      },
-      handleEventResize(e){
-
       },
       drop(e){
-        console.log(e)
-      }
+        let that = this
+        var params = {
+          tableName: "scheduledate",
+          DateType:e.draggedEl.innerText,
+          WorkDate:e.dateStr,
+          color:e.draggedEl.attributes.color.value
+        }
+        that.axios.post("/api/CUID",that.qs.stringify(params)).then(res =>{
+          if(res.data == "OK"){
+            that.$message({
+              showClose: true,
+              type: 'success',
+              message: "添加成功"
+            });
+            that.getScheduling()
+          }else{
+            that.$message({
+              type: 'info',
+              message: res.data
+            });
+          }
+        })
+      },
+      handleDateClick(e) {  //点击日期
+
+      },
+      handleEventClick(e){  //点击日历里的日程 进行删除
+        let that = this
+        var ID = {
+          id:e.event.extendedProps.ID
+        }
+        that.$confirm('此操作将永久删除该日程, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          that.axios.delete("/api/CUID",{
+            params: {
+              tableName: "scheduledate",
+              delete_data:JSON.stringify(ID),
+            }
+          }).then(res =>{
+            if(res.data == "OK"){
+              that.$message({
+                type: 'success',
+                message: '删除成功!'
+              });
+              that.getScheduling()
+            }
+          },res =>{
+            console.log("请求错误")
+          })
+        }).catch(() => {
+          that.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      },
+      handleEventDrop(e){ //日历里拖拽换位日程 进行修改
+        let that = this
+        var startDate = moment(e.event.start).format('YYYY-MM-DD')
+        var params = {
+          tableName: "scheduledate",
+          ID:e.event.extendedProps.ID,
+          DateType:e.event.title,
+          WorkDate:startDate,
+          color:e.event.backgroundColor
+        }
+        that.axios.put("/api/CUID",that.qs.stringify(params)).then(res =>{
+          if(res.data == "OK"){
+            that.$message({
+              showClose: true,
+              type: 'success',
+              message: '修改成功'
+            });
+            that.getScheduling()
+          }else{
+            that.$message({
+              type: 'info',
+              message: res.data
+            });
+          }
+        })
+      },
     }
   }
 </script>
