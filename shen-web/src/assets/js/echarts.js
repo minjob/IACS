fd0aee04f4373be53ee305f13518cbd0c8b062f1
ligang <2228695510@qq.ccom>
2020-07-04 10:01:37 +0800

    数据管理侧边

--- shen-web/src/assets/js/echarts.js
+++ shen-web/src/assets/js/echarts.js
@@ -2,8 +2,10 @@
   
  // 再引入需要使用的图表类型，标题，提示信息等
  import 'echarts/lib/chart/bar'
+ import 'echarts/lib/chart/line'
  import 'echarts/lib/component/legend'
  import 'echarts/lib/component/title'
  import 'echarts/lib/component/tooltip'
-  
+ import 'echarts/lib/component/dataZoom'
+ import 'echarts/lib/component/grid'
  export default echarts
--- shen-web/src/views/energy/DataManagement.vue
+++ shen-web/src/views/energy/DataManagement.vue
@@ -10,39 +10,19 @@
       <el-col :span="24">
         <div class="mainShow">
             <el-container style="height: 887px;">
-                <el-aside width="200px" style="background-color:rgba(52, 56, 62, 1)">
+                <el-aside width="290px" style="background-color:rgba(52, 56, 62, 1)">
                   <el-menu :default-openeds="['1']">
-                    <el-submenu index="1">
-                      <template slot="title"><i class="el-icon-message"></i>导航一</template>
+                    <el-submenu  v-for="item in menuTagName" :key="item.ID" :index="1">
+                      <template slot="title"><i class="el-icon-price-tag"></i>{{item.ParentTagName}}</template>
                       <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="1-1">选项1</el-menu-item>
-                        <el-menu-item index="1-2">选项2</el-menu-item>
+                        <el-menu-item index="1-1">
+                           <el-checkbox v-model="checked1" label="备选项1" :border="false" @change=selectChange()></el-checkbox>
+                        </el-menu-item>
                       </el-menu-item-group>
-                      <el-menu-item-group title="分组2">
-                        <el-menu-item index="1-3">选项3</el-menu-item>
-                      </el-menu-item-group>
-                    </el-submenu>
-                    <el-submenu index="2">
-                      <template slot="title"><i class="el-icon-menu"></i>导航二</template>
                       <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="2-1">选项1</el-menu-item>
-                        <el-menu-item index="2-2">选项2</el-menu-item>
-                      </el-menu-item-group>
-                      <el-menu-item-group title="分组2">
-                        <el-menu-item index="2-3">选项3</el-menu-item>
-                      </el-menu-item-group>
-                    </el-submenu>
-                    <el-submenu index="3">
-                      <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-                      <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="3-1">选项1</el-menu-item>
-                        <el-menu-item index="3-2">选项2</el-menu-item>
-                      </el-menu-item-group>
-                      <el-menu-item-group title="分组2">
-                        <el-menu-item index="3-3">选项3</el-menu-item>
+                        <el-menu-item index="1-1">
+                           <el-checkbox v-model="checked2" label="备选项2" :border="false"></el-checkbox>
+                        </el-menu-item>
                       </el-menu-item-group>
                     </el-submenu>
                   </el-menu>
@@ -50,7 +30,7 @@
   
             <el-container>
               <el-header style="text-align: right; font-size: 12px; height:100px;">                  
-               <el-row :gutter="30">
+               <el-row v-if="controldate">
                   <el-col :span="4">
                     <div class="block">
                         <span class="demonstration">设置时间1</span>
@@ -106,13 +86,36 @@
                         </el-date-picker>
                     </div>
                   </el-col>
+                  <el-col :span="2">
+                    <div class="confirmButton">
+                      <el-button type="primary">确认选择</el-button>
+                    </div>
+                  </el-col>
                 </el-row>
+                <el-row v-if="!controldate">
+                  <el-col :span="4">
+                    <div class="block">
+                        <span class="demonstration">设置时间1</span>
+                        <el-date-picker
+                          v-model="value1"
+                          type="datetime"
+                          placeholder="选择日期时间"
+                          default-time="12:00:00">
+                        </el-date-picker>
+                    </div>
+                  </el-col>
+                  <el-col :span="2">
+                    <div class="confirmButton">
+                      <el-button type="primary">确认选择</el-button>
+                    </div>
+                  </el-col>
+                  </el-row>
               </el-header>
               
               <el-main>
                 <div class="myechart">
                   <div id="mainechart" style="width:100%;height:100%;">
-
+                    
                   </div>
                 </div>
               </el-main>
@@ -131,11 +134,16 @@ import echarts from '@/assets/js/echarts.js'
     name: "Home",
     data(){
       return {
+        //时间绑定数值
         value1:'',
         value2:'',
         value3:'',
         value4:'',
         value5:'',
+        menuTagName:[],
+        controldate:true,
+        checked1:true,
+        checked2:true,//多选框的值绑定
         navOptionsCurrent:1,
         navOptions:[
             {name:"趋势分析",value:1},
@@ -144,8 +152,10 @@ import echarts from '@/assets/js/echarts.js'
         TagCode:''
       }
     },
-    mounted(){
+    created(){
       this.getValue()
+    },
+    mounted(){
       this.drawLine()
     },
     watch:{
@@ -155,48 +165,164 @@ import echarts from '@/assets/js/echarts.js'
 
     },
     methods: {
+      selectChange(){
+        if(this.checked1 && this.checked2){
+          this.controldate=true
+        }else{
+          this.controldate=false
+        }
+      },
       drawLine(){
       var myChart = echarts.init(document.getElementById('mainechart'));
       // 绘制图表
-      var option={
-        title: {
-        text: 'ECharts 入门示例'
-    },
-    tooltip: {},
-    xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    },
-    yAxis: {},
-    series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
-      }
+      var option = {
+          backgroundColor: '#21202D',//整个图的背景颜色
+          legend: {
+          data: ['xxx','xxx','xxx','xxx'],
+          inactiveColor: '#777',
+          textStyle: {
+            color: '#fff'
+        }
+      },
+          tooltip: { //提示框配置
+          trigger: 'axis',
+          axisPointer: {
+          animation: false, //是否开启平移线颜色
+          type: 'cross',//指示器类型，十字准星
+          lineStyle: {  //提示框竖直线颜色
+              color: '#555',
+              width: 2,
+              opacity: 1
+            }
+        }
+      },
+          toolbox: {
+            feature: {
+                dataZoom: {
+                    yAxisIndex: false
+                },
+                brush: {
+                    type: ['lineX', 'clear']
+                }
+            }
+        },
+          brush: {
+            xAxisIndex: 'all',
+            brushLink: 'all',
+            outOfBrush: {
+                colorAlpha: 0.1
+            }
+        },
+          xAxis: {
+            type: 'category',
+            data: dates,
+            axisLine: { lineStyle: { color: '#8392A5' } } // x轴颜色
+        },
+          yAxis: {
+          scale: true,
+          axisLine: { lineStyle: { color: '#8392A5' } },
+          splitLine: { show: false }
+        },
+          grid: { //图形界面距离上下左右的设置
+            bottom: 80,
+            top:20
+        },
+          dataZoom: [{
+          textStyle: {
+            color: '#fff'
+        },
+          handleSize: '60%',
+          dataBackground: {
+            areaStyle: {
+                color: '#fff'//底部滑动条背景颜色
+            },
+            lineStyle: {
+                opacity: 0.8,
+                color: '#8392A5'//滑动栏内部线条颜色
+            }
+        },
+          handleStyle: {
+            color: '#fff',
+            shadowBlur: 1,
+            shadowColor: '#fff',
+            shadowOffsetX: 2,
+            shadowOffsetY: 2
+        }
+    }, {
+        type: 'slider',  //slider表示有滑动块的，inside表示内置的
+        dataBackground:'#fff',
+
+    }],
+    animation: false,
+    series: [ // 驱动图表生成的数据内容数组，几条折现，数组中就会有几个对应对象，来表示对应的折线
+        {
+            name: name1,
+            type: 'line', // pie->饼状图  line->折线图  bar->柱状图
+            data: this.calculateMA(5, data),//5天内的收盘价之和/5  红线
+            smooth: true,
+            showSymbol: false,
+            lineStyle: {
+                width: 1
+            }
+        },
+        {
+            name: name2,
+            type: 'line',
+            data: this.calculateMA(10, data),//10天内的收盘价之和/10  灰线
+            showSymbol: false,
+            lineStyle: {
+                width: 1
+            }
+        },
+        {
+            name: name3,
+            type: 'line',
+            data: this.calculateMA(20, data),//20天内的收盘价之和/20 蓝线
+            smooth: true, 
+            showSymbol: false,
+            lineStyle: {
+                width: 1
+            }
+        },
+        {
+            name: name4,
+            type: 'line',
+            data: this.calculateMA(30, data),//30天内的收盘价之和/30 橙线
+            smooth: true,
+            showSymbol: false,
+            lineStyle: {
+                width: 1
+            }
+        },
+         {
+            name: name5,
+            type: 'line',
+            data: this.calculateMA(40, data),//30天内的收盘价之和/30 橙线
+            smooth: true,
+            showSymbol: false,
+            lineStyle: {
+                width: 1
+            }
+        }
+    ]
+};
       myChart.setOption(option)
       },
       showPage(index) {
         this.navOptionsCurrent = index
-      },
+},
       getValue(){
         var params = {
-          tableName:'TagMaintain',
+          tableName:'ParentTagMaintain',
           limit:1000,
           offset:0
         }
-        var params1={
-          begin:'2020-06-15 00:00:00',
-          end:'2020-06-15 16:00:00',
-          TagCode:this.TagCode
-        }
         this.axios.get('/api/CUID',{params}).then((res) => {
-          var alldata=JSON.parse(res.data).rows[0].TagCode
-          console.log(alldata)
-          this.TagCode=alldata
+          console.log(JSON.parse(res.data))
+          this.menuTagName=JSON.parse(res.data).rows
+
         })
-        this.axios.get('/api/energytrendtu',{params:{params1}}).then((value) => {
-          console.log(value)
-        })
+       
       }
     }
   }
@@ -236,7 +362,8 @@ import echarts from '@/assets/js/echarts.js'
   line-height: 60px;
 }
 .mainShow .el-main{
-  padding: 0;
+  overflow: hidden;
+  padding: 0px;
 }
 .myechart{
   width: 100%;
@@ -246,16 +373,18 @@ import echarts from '@/assets/js/echarts.js'
 .row-bg{
   background-color: #ccc;
 }
-
-.el-col,.el-col-4{
-  margin-right: 10px;
-  padding-left: 0px;
-  padding-right: 0px;
-}
 .mainShow .el-submenu__title, .el-submenu__title:hover {
   background: #34383E;
   color: #fff;
   font-size: 18px;
 }
-
+.el-date-editor.el-input{
+  width: 180px;
+}
+.confirmButton{
+  width: 100%;
+  height: 100%;
+  padding-top: 16px;
+  padding-left: 17px;
+}
 </style>
