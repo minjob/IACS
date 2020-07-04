f3d11881c9381065a0a2605690ed2be0ad74bbeb
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2020-07-04 10:53:36 +0800

    no message

--- handlers/ERP_Scheduling/ERP_Schedul.py
+++ handlers/ERP_Scheduling/ERP_Schedul.py
@@ -247,7 +247,7 @@ def addscheduledates():
                     sc.color = color
                     db_session.add(sc)
                     db_session.commit()
-                db_session.remove()
+                db_session.close()
             return 'OK'
         except Exception as e:
             db_session.rollback()
