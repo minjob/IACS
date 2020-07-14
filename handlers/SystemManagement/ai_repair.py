ac187acb7082676c2b8b816b3be64034e0ba6015
HuangXiongjin <1975436224@qq.com>
2020-07-14 12:06:55 +0800

    修改了保养周期接口

--- handlers/SystemManagement/ai_repair.py
+++ handlers/SystemManagement/ai_repair.py
@@ -48,7 +48,7 @@ def get_time_stamp(work_time):
     """
     time_array = time.strptime(work_time, "%Y-%m-%d %H:%M:%S")
     time_stamp = int(time.mktime(time_array))
-    return 0 < time_stamp - int(time.time()) < 604800*2
+    return 0 < time_stamp - int(time.time()) < 604800*4
 
 
 def get_no(no):
@@ -126,7 +126,7 @@ def keep_plans():
     work_type = json_data.get('Type')
     week_time = '单次' if work_type == '单次' else json_data.get('WeekTime')
     data = KeepPlan(EquipmentCode=json_data.get('EquipmentCode'), No=get_no(json_data.get('ApplyTime')),
-                    Worker=current_user.Name, ApplyTime=json_data.get('ApplyTime'), Type=json_data.get('Type'),
+                    Worker='current_user.Name', ApplyTime=json_data.get('ApplyTime'), Type=json_data.get('Type'),
                     StartTime=json_data.get('StartTime'), Describe=json_data.get('Describe'),
                     WorkTime=work_time, WeekTime=week_time)
     db_session.add(data)
@@ -141,19 +141,19 @@ def keep_tasks():
     query_data = db_session.query(KeepPlan).filter_by(Status='待保养').all()
     if request.method == 'GET':
         # 每页多少条
-        limit = int(request.values.get('limit'))
+        limit = int(request.values.get('limit', '5'))
         # 当前页
-        offset = int(request.values.get('offset'))
+        offset = int(request.values.get('offset', '1'))
         for item in query_data:
-            q = db_session.query(KeepTask).filter_by(No=item.No).first()
-            if not q and get_time_stamp(item.WorkTime):
+            # q = db_session.query(KeepTask).filter_by(No=item.No).first()
+            if get_time_stamp(item.WorkTime):
                 data = KeepTask(EquipmentCode=item.EquipmentCode, No=item.No, Worker=item.Worker, Status=item.Status,
                                 ApplyTime=item.ApplyTime, StartTime=item.StartTime, WorkTime=item.WorkTime,
                                 WeekTime=item.WeekTime, Type=item.Type)
                 db_session.add(data)
                 db_session.commit()
             if item.Type == '单次':
-                db_session.delete(q)
+                db_session.delete(item)
                 db_session.commit()
         data = db_session.query(KeepTask).order_by(KeepTask.ApplyTime.desc()).limit(limit).offset((offset - 1) * limit)
         total = db_session.query(KeepTask).count()
@@ -166,8 +166,10 @@ def keep_tasks():
         content = json_data.get('Content')
         item = db_session.query(KeepTask).filter_by(No=no).first()
         data = KeepRecord(EquipmentCode=item.EquipmentCode, No=no, Worker=item.Worker, Status='已完成', Type=item.Type,
-                          KeepWorker=current_user.Name, ApplyTime=item.ApplyTime, StartTime=item.StartTime,
+                          KeepWorker='current_user.Name', ApplyTime=item.ApplyTime, StartTime=item.StartTime,
                           Describe=item.Describe, Content=content, WeekTime=item.WeekTime, EndTime=end_time)
+        db_session.delete(item)
+        db_session.commit()
         keep_plan = db_session.query(KeepPlan).filter_by(No=no).first()
         if keep_plan:
             keep_plan.WorkTime = add_date(keep_plan.WeekTime, keep_plan.WorkTime)
