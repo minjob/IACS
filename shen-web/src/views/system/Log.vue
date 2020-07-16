94bbea27eba9f98d6870a1b4ddf0aed4be106155
lenux-PC\lenux <minwork0926@163.com>
2020-07-16 14:05:30 +0800

    no message

--- shen-web/src/views/system/Log.vue
+++ shen-web/src/views/system/Log.vue
@@ -4,8 +4,13 @@
       <div class="page-title">
         <span style="margin-left: 10px;" class="text-size-normol">系统日志</span>
       </div>
-      <div class="platformContainer">
-        <tableView class="blackComponents" :tableData="TableData" @getTableData="getTableData"></tableView>
+      <div class="platformContainer blackComponents">
+        <el-form :inline="true" :model="formParameters">
+          <el-form-item label="选择查询时间">
+            <el-date-picker type="date" v-model="formParameters.searchDate" size="mini" format="yyyy-MM-dd" style="width: 130px;" :clearable="false" @change="searchLogDate"></el-date-picker>
+          </el-form-item>
+        </el-form>
+        <tableView class="blackComponents" :tableData="TableData" @getTableData="searchLogDate"></tableView>
       </div>
     </el-col>
   </el-row>
@@ -13,6 +18,7 @@
 
 <script>
   import tableView from '@/components/CommonTable'
+  var moment = require('moment');
   export default {
     name: "Log",
     components:{tableView},
@@ -26,26 +32,29 @@
             {prop:"ComputerName",label:"计算机名称",showField:true,searchProp:false},
             {prop:"UserName",label:"操作用户",showField:true,searchProp:true},
             {prop:"OperationDate",label:"操作日期",showField:true,searchProp:true},
-            {prop:"OperationContent",label:"操作内容",showField:true,searchProp:false},
-            {prop:"OperationType",label:"类型",showField:true,searchProp:true},
+            {prop:"OperationContent",width:"400",label:"操作内容",showField:true,searchProp:false},
+            {prop:"OperationType",width:"100",label:"类型",showField:true,searchProp:true},
           ],
           data:[],
           limit:5,
           offset:1,
           total:0,
-          searchProp:"",
-          searchVal:"",
         },
+        formParameters:{
+          searchDate:moment().format("YYYY-MM-DD")
+        }
       }
     },
     mounted() {
-      this.getTableData()
+      this.searchLogDate()
     },
     methods:{
-      getTableData(){
+      searchLogDate(){
         var that = this
         var params = {
           tableName: this.TableData.tableName,
+          field:"OperationDate",
+          fieldvalue:moment(this.formParameters.searchDate).format("YYYY-MM-DD"),
           limit:this.TableData.limit,
           offset:this.TableData.offset - 1
         }
@@ -58,7 +67,7 @@
         },res =>{
           console.log("请求错误")
         })
-      },
+      }
     }
   }
 </script>
