ea50d2c0d0423d00668deefcbf5152627a162be4
HuangXiongjin <1975436224@qq.com>
2020-07-20 17:28:38 +0800

    修改了新的能耗趋势接口接口

--- handlers/SystemManagement/monitor.py
+++ handlers/SystemManagement/monitor.py
@@ -1,5 +1,6 @@
 import json
 import time
+from datetime import datetime
 
 import redis
 from flask import Blueprint, request
@@ -479,7 +480,8 @@ def energy_trends():
                       " SampleTime between " + "'" + Begin + "'" + " and " + "'" + End + "'"
                 results = db_session.execute(sql).fetchall()
                 for result in results[::6]:
-                    data.append({'time': time.strftime("%Y-%m-%d %H:%M:%S", result['time']), 'value': result['value']})
+                    data.append({'time': datetime.strftime(result['time'], "%Y-%m-%d %H:%M:%S"),
+                                 'value': result['value']})
             return json.dumps({'code': '20001', 'message': '成功', 'data': data}, cls=AlchemyEncoder, ensure_ascii=False)
         else:
             # 一个tag查询多天
