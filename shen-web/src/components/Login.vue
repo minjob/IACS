2761b51953658ddf7f003b4d3f7717c92af061ba
lenux-PC\lenux <minwork0926@163.com>
2020-07-27 13:30:14 +0800

    no message

--- shen-web/src/components/Login.vue
+++ shen-web/src/components/Login.vue
@@ -1,34 +1,36 @@
 <template>
   <el-container style="height: 100%;">
-    <div class="login-img-bg">
-      <div class="login-mask-bg"></div>
-      <div class="login-form-box">
-        <div class="login-form-title">能耗智能分析控制系统</div>
-        <div class="login-form-mask"></div>
-        <div class="login-form">
-          <el-form ref="ruleForm" :model="loginForm" :rules="rules" style="width: 100%;">
-            <el-form-item prop="WorkNumber">
-              <el-input placeholder="请输入工号" v-model="loginForm.WorkNumber">
-                <i slot="prefix" class="el-input__icon el-icon-s-custom"></i>
-              </el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-              <el-input placeholder="请输入密码" v-model="loginForm.password" show-password>
-                <i slot="prefix" class="el-input__icon el-icon-lock"></i>
-              </el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-checkbox v-model="loginForm.rememb" class="remembCheckbox">记住密码</el-checkbox>
-            </el-form-item>
-          </el-form>
+    <div id="loginBgCanvas">
+      <div class="login-center-bg">
+        <div class="login-form-box">
+          <div class="login-form-title">桃园地铁站智能分析控制系统</div>
+          <div class="login-form-mask"></div>
+          <div class="login-form">
+            <el-form ref="ruleForm" :model="loginForm" :rules="rules" style="width: 100%;">
+              <el-form-item prop="WorkNumber">
+                <el-input placeholder="请输入工号" v-model="loginForm.WorkNumber">
+                  <i slot="prefix" class="el-input__icon el-icon-s-custom"></i>
+                </el-input>
+              </el-form-item>
+              <el-form-item prop="password">
+                <el-input placeholder="请输入密码" v-model="loginForm.password" show-password>
+                  <i slot="prefix" class="el-input__icon el-icon-lock"></i>
+                </el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-checkbox v-model="loginForm.rememb" class="remembCheckbox">记住密码</el-checkbox>
+              </el-form-item>
+            </el-form>
+          </div>
+          <div class="login-form-submit" :class="isSubLoadding?'submitLoadding':''" @click="submitForm('ruleForm')">登录</div>
         </div>
-        <div class="login-form-submit" :class="isSubLoadding?'submitLoadding':''" @click="submitForm('ruleForm')">登录</div>
       </div>
     </div>
   </el-container>
 </template>
 
 <script>
+  import * as Three from 'three'
   export default {
     name: "login",
     data(){
@@ -48,10 +50,22 @@
           ]
         },
         isSubLoadding:false,
+        camera: null,
+        scene: null,
+        renderer: null,
+        mesh: null,
+        SEPARATION: 100, AMOUNTX: 50, AMOUNTY: 50,
+        container:null,
+        particles:null,count:0,
+        mouseX:0,mouseY:0,
+        windowHalfX: window.innerWidth / 2,
+        windowHalfY : window.innerHeight  / 2,
       }
     },
     mounted(){
       this.getCookie()
+      this.init()
+      this.animate()
     },
     methods:{
       submitForm(formName){
@@ -123,37 +137,135 @@
       },
       clearCookie:function () {
         this.setCookie("","","",-1);
+      },
+      init: function () {
+        this.container = document.getElementById('loginBgCanvas')
+        this.camera = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 )
+        this.camera.position.z = 1000
+        this.scene = new Three.Scene()
+        this.scene.fog = new Three.FogExp2( 0xffffff, 0.0008 );
+
+        var numParticles = this.AMOUNTX * this.AMOUNTY;
+				var positions = new Float32Array( numParticles * 3 );
+				var scales = new Float32Array( numParticles );
+				var i = 0, j = 0;
+				for ( var ix = 0; ix < this.AMOUNTX; ix ++ ) {
+					for ( var iy = 0; iy < this.AMOUNTY; iy ++ ) {
+						positions[ i ] = ix * this.SEPARATION - ( ( this.AMOUNTX * this.SEPARATION ) / 2 ); // x
+						positions[ i + 1 ] = 0; // y
+						positions[ i + 2 ] = iy * this.SEPARATION - ( ( this.AMOUNTY * this.SEPARATION ) / 2 ); // z
+						scales[ j ] = 1;
+						i += 3;
+						j ++;
+					}
+				}
+
+        var geometry = new Three.BufferGeometry();
+				geometry.setAttribute( 'position', new Three.BufferAttribute( positions, 3 ) );
+				geometry.setAttribute( 'scale', new Three.BufferAttribute( scales, 1 ) );
+				var material = new Three.ShaderMaterial( {
+					uniforms: {
+						color: { value: new Three.Color( 0x097bdb ) },
+					},
+					vertexShader: document.getElementById( 'vertexshader' ).textContent,
+					fragmentShader: document.getElementById( 'fragmentshader' ).textContent
+				});
+
+				this.particles = new Three.Points( geometry, material );
+				this.scene.add( this.particles );
+
+				this.renderer = new Three.WebGLRenderer( { antialias: true } );
+				this.renderer.setPixelRatio( window.devicePixelRatio );
+				this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.container.appendChild(this.renderer.domElement)
+
+
+				document.addEventListener( 'mousemove', this.onDocumentMouseMove, false );
+				document.addEventListener( 'touchstart', this.onDocumentTouchStart, false );
+				document.addEventListener( 'touchmove', this.onDocumentTouchMove, false );
+
+				window.addEventListener( 'resize', this.onWindowResize, false );
+      },
+      animate: function () {
+        requestAnimationFrame( this.animate );
+				this.render();
+      },
+      onWindowResize(){
+        this.windowHalfX = window.innerWidth / 2;
+				this.windowHalfY = window.innerHeight / 2;
+				this.camera.aspect = window.innerWidth / window.innerHeight;
+				this.camera.updateProjectionMatrix();
+				this.renderer.setSize( window.innerWidth, window.innerHeight );
+      },
+      onDocumentMouseMove( event ){
+        this.mouseX = event.clientX - this.windowHalfX;
+				this.mouseY = event.clientY - this.windowHalfY;
+      },
+      onDocumentTouchStart( event ){
+        if ( event.touches.length === 1 ) {
+					event.preventDefault();
+					this.mouseX = event.touches[ 0 ].pageX - this.windowHalfX;
+					this.mouseY = event.touches[ 0 ].pageY - this.windowHalfY;
+				}
+      },
+      onDocumentTouchMove(event){
+        if ( event.touches.length === 1 ) {
+					event.preventDefault();
+					this.mouseX = event.touches[ 0 ].pageX - this.windowHalfX;
+					this.mouseY = event.touches[ 0 ].pageY - this.windowHalfY;
+				}
+      },
+      render(){
+        this.camera.position.x += ( this.mouseX - this.camera.position.x ) * .05;
+				this.camera.position.y += ( - this.mouseY - this.camera.position.y ) * .05;
+				this.camera.lookAt( this.scene.position );
+				var positions = this.particles.geometry.attributes.position.array;
+				var scales = this.particles.geometry.attributes.scale.array;
+				var i = 0, j = 0;
+				for ( var ix = 0; ix < this.AMOUNTX; ix ++ ) {
+					for ( var iy = 0; iy < this.AMOUNTY; iy ++ ) {
+						positions[ i + 1 ] = ( Math.sin( ( ix + this.count ) * 0.3 ) * 50 ) +
+										( Math.sin( ( iy + this.count ) * 0.5 ) * 50 );
+						scales[ j ] = ( Math.sin( ( ix + this.count ) * 0.3 ) + 1 ) * 8 +
+										( Math.sin( ( iy + this.count ) * 0.5 ) + 1 ) * 8;
+						i += 3;
+						j ++;
+					}
+				}
+				this.particles.geometry.attributes.position.needsUpdate = true;
+				this.particles.geometry.attributes.scale.needsUpdate = true;
+				this.renderer.render( this.scene, this.camera );
+				this.count += 0.1;
       }
     }
   }
 </script>
 
 <style scoped>
-  .login-img-bg{
+  #loginBgCanvas{
     position: relative;
     width: 100%;
     height: 100%;
-    /*background: url("../assets/imgs/loginBg.jpg") no-repeat;*/
-    background-size:cover;
-    -webkit-background-size: cover;
-    -o-background-size: cover;
-    background-position: center 0;
-    display: flex;
-    align-items:center;
   }
-  .login-mask-bg{
+  #loginBgCanvas canvas{
+    /*设置display 为 block 可以消除下方5px 的边距!!!*/
+    display: block!important;
+    width: 100%!important;
+    height: 100% !important;
+  }
+  .login-center-bg{
     position: absolute;
     top: 0;
     width: 100%;
     height: 100%;
-    background: #082F4C;
-    opacity: 0.52;
+    display: flex;
+    align-items:center;
   }
   .login-form-box{
     position: relative;
     width: 400px;
     height: 360px;
-    margin: 0 auto;
+    margin: 0 auto 50px auto;
   }
   .login-form-mask{
     position: absolute;
